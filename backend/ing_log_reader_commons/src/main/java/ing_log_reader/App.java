d6b7b528edab7f0f9ad895d48d1e0dc7dc152a10
nancho313 <nancho313@d6b97766-3fdf-4c47-8c44-58d991f71a42>
2017-12-02 21:57:04 +0000

    Proyecto commons

---
+++ backend/ing_log_reader_commons/src/main/java/ing_log_reader/App.java
@@ -0,0 +1,13 @@
+package ing_log_reader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
